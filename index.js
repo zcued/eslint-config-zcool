97baa270010d57bd67c5a01ff7a015e37f021e1b
zhanghao <zhanghao@zcool.com.cn>
2018-08-28 12:31:36 +0800

    MOD: update comma-dangle to always-multiline

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ module.exports = {
     // https://eslint.org/docs/rules/comma-dangle
     "comma-dangle": [
       "error",
-      "only-multiline",
+      "always-multiline",
       {"functions": "never"}
     ],
 
