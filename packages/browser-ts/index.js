fc3acd1d5d6cf259226d59110f8ed4d303b5c3f8
zhangguangzhi <zhangguangzhi@zcool.com.cn>
2021-06-30 14:50:11 +0800

    使用lerna管理

---
+++ packages/browser-ts/index.js
@@ -0,0 +1,27 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react'],
+  extends: [
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    '../../rules/base',
+    '../../rules/react',
+    '../../rules/import',
+    '../../rules/ts',
+  ],
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020, //  Allows for the parsing of modern ECMAScript features
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {},
+}
---
+++ packages/browser/index.js
@@ -0,0 +1,33 @@
+module.exports = {
+  parser: 'babel-eslint',
+  plugins: ['prettier', 'react', 'react-hooks'],
+  extends: ['airbnb-base', 'prettier', '../../rules/base', '../../rules/react', '../../rules/import'],
+  globals: {
+    window: true,
+    document: true,
+    navigator: true,
+  },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+    es6: true,
+  },
+  settings: {
+    polyfills: ['fetch', 'promises'],
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+  parserOptions: {
+    ecmaVersion: 2020, //  Allows for the parsing of modern ECMAScript features
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      legacyDecorators: true,
+      experimentalObjectRestSpread: true,
+    },
+  },
+  rules: {},
+}
